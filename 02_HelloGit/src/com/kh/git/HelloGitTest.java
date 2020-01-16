66f8e897973805501d72a8f21c718b08453c711e
CHO <CHO@DESKTOP-RONJRE2>
2020-01-16 19:37:47 +0900

    안녕

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -7,6 +7,5 @@ public static void main(String[] args) {
 	System.out.println("허파고 바보");
 	System.out.println("조정우는 천재");
 	System.out.println("조개일은 고스톱쟁이");
-	System.out.println("조개일은 고스톱쟁이");
 }
 }
